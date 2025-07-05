573995dc7d77e3f2918449a7695240ec1358dfdc
fut71 <130661524+fut71@users.noreply.github.com>
2025-07-05 23:53:31 +0900

    [level 0] Title: 세균 증식, Time: 0.02 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(int n, int t) 
+{
+    int answer = n * pow(2, t);
+        
+    return answer;
+}
