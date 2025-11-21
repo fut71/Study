498c07187a74a2d7c300f1340b40a31789ed5ada
fut71 <130661524+fut71@users.noreply.github.com>
2025-11-21 18:24:10 +0900

    [level 2] Title: H-Index, Time: 0.04 ms, Memory: 4.02 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int solution(vector<int> citations) 
+{
+    int answer = 0;
+        
+    sort(citations.begin(), citations.end(), greater<int>());
+    
+    for(size_t i = 0; i < citations.size(); ++i)
+    {
+        if(i + 1 <= citations[i])
+        {
+            answer = i + 1;
+        }
+        else
+        {
+            break;
+        }
+    }
+    
+    return answer;
+}
